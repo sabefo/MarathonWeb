9df6faac05b101f7e3c4add47e082dfe96bc20ae
codeamac02 <codeamac02@gmail.com>
2015-06-26 10:40:05 -0500

    Add Users model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+
+  def self.authenticate(email, password)
+		return nil unless user = User.find_by(email: email)
+
+	    # si el email y el password corresponden a un usuario valido, regresa el usuario
+	    # de otra manera regresa nil
+			if user.password == password
+		    user
+		  else
+		  	nil
+		  end
+		end
+end
