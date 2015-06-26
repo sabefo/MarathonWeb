8c9ab98d9694e3dc2d0f6814d47df670a30fdf81
codeamac01 <codeamac01@gmail.com>
2015-06-26 11:09:13 -0500

    Cards migration

--- db/migrate/20150626103428_create_cards.rb
+++ db/migrate/20150626103428_create_cards.rb
@@ -1,4 +1,10 @@
 class CreateCards < ActiveRecord::Migration
   def change
+  	create_table :cards do |t|
+	    t.string :question
+	    t.string :answer
+	    t.belongs_to :deck
+
+	    t.timestamps
   end
 end
--- db/migrate/20150626103514_create_decks.rb
+++ db/migrate/20150626103514_create_decks.rb
@@ -2,5 +2,7 @@ class CreateDecks < ActiveRecord::Migration
   def change
   	create_table :decks do |t|
 	    t.string :category
+
+	    t.timestamps
   end
 end
